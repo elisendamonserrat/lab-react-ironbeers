04321f8280159c21441b226ed9dfcf39cd8a4107
Elisenda Monserrat <elisenda.monserrat@gmail.com>
2021-06-26 15:34:49 +0200

    adds beer list cards with styles

---
+++ src/components/BeerCard.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+
+function BeerCard(props) {
+    const creatorNameCleaned = (creator) => {
+        const symbol = creator.indexOf('<');
+        const cleanedName = creator.slice(0, symbol)
+        return cleanedName
+    }
+    
+    const { image_url, name, tagline, contributed_by } = props.beerDetails;
+
+    return (
+        <section className="border w-full h-1/8 my-2 flex items-center">
+            <div className="object-contain w-1/4 px-8 py-4">
+                <img className="object-contain w-4/5" src={image_url} alt={name} />
+            </div>
+            <div className="mx-4 my-2 w-3/4">
+                <h1 className="text-lg text-black font-semibold">{name}</h1>
+                <p className="text-base font-medium text-gray-500">{tagline}</p>
+                <p><strong>Created by:</strong> {creatorNameCleaned(contributed_by)}</p>
+            </div>
+        </section>
+    )
+}
+
+export default BeerCard
--- src/views/BeersPage.jsx
+++ src/views/BeersPage.jsx
@@ -1,21 +1,45 @@
 import React, { Component } from 'react'
 import Header from '../components/Header'
+import axios from 'axios';
+import BeerCard from '../components/BeerCard';
+
 
 export class BeersPage extends Component {
-
+    constructor(props) {
+        super(props);
+        this.state = {
+            beersList: [],
+            status: 'loading'
+        }
+    }
 
     componentDidMount () {
-        
+        axios
+            .get("https://ih-beers-api2.herokuapp.com/beers")
+            .then(response => {
+                this.setState({
+                    beersList: response.data,
+                    status: 'loaded'
+                })
+            })
+            .catch((e) => console.log(e))
     }
 
 
     render() {
+        const { beersList, status } = this.state;
+
         return (
             <>
-                <Header />
-                <p>This is all the beers page</p>
-                
-                
+             <Header />
+             <main className="w-11/12 mx-auto">
+                { status === 'loading' && <p>Data is loading...</p>}
+                { status === 'loaded' && beersList.map((beer, index) => {
+                    return (
+                        < BeerCard  beerDetails={beer} key={index}/>
+                    )
+                })}
+             </main>
             </>
         )
     }
